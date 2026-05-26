12bfa7acf3d284a3b35d43ab87324cb26547ec9e
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-05-26 14:37:43 +0800

    feature: connect database to mongoDB

--- server/server.js
+++ server/server.js
@@ -2,14 +2,20 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+app.get("/", (req, res) => {
+	res.send("Server is running");
+});
+
 app.listen(port, () => {
-	console.log(`Server started on port: ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
