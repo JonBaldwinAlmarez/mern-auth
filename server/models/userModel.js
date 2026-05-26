9cc802c9dc64bda42883366e2fe98ea9c64b4558
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-05-26 15:58:25 +0800

    chore: create user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -10,3 +10,8 @@ const userSchema = new mongoose.Schema({
 	resetOtp: { type: String, default: "" },
 	resetOtpExpireAt: { type: Number, default: 0 },
 });
+
+//  Create User Model
+const usermodel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default usermodel;
