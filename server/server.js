2087a327f89f0f7ae7fd1616d7a33f55aae310d0
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-05-27 14:29:19 +0800

    fix: server.js and authroutes typos

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authroutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,9 +13,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+/* API end points */
 app.get("/", (req, res) => {
 	res.send("Server is running");
 });
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
