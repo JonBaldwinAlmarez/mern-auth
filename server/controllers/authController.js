81bd9d0d7fe40b608542599f2c90253d3b578ebf
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-05-27 10:25:42 +0800

    chore: create controller for user login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,7 +35,6 @@ export const register = async (req, res) => {
 		await user.save();
 
 		/* Create token */
-
 		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
 			expiresIn: "7d",
 		});
@@ -47,7 +46,60 @@ export const register = async (req, res) => {
 			sameSite: process.env.NODE_ENV === "production" ? "none" : "strick",
 			maxAge: 7 * 24 * 60 * 60 * 1000,
 		});
+
+		return res.json({ success: true });
 	} catch (error) {
 		res.json({ success: false, message: error.message });
+	}
+};
+
+// Controller function for user login
+
+export const login = async (req, res) => {
+	// Get email and password in req body
+	const { email, password } = req.body;
+
+	// email and password validation
+	if (!email || !password) {
+		return res.json({
+			success: false,
+			message: "Provide Email and Password",
+		});
+	}
+
+	try {
+		const user = await userModel.findOne({ email });
+
+		if (!user) {
+			return res.json({ success: false, message: "Invalid Email" });
+		}
+
+		// Get password in DB and Match w/ Password provided
+		const isMatch = await bcrypt.compare(password, user.password);
+
+		if (!isMatch) {
+			return res.json({ success: false, message: "Invalid Password" });
+		}
+
+		/* Create token */
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+			expiresIn: "7d",
+		});
+
+		// Send token to user in res and add cookie
+		res.cookie("token", token, {
+			// Send Property
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: process.env.NODE_ENV === "production" ? "none" : "strick",
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+		});
+
+		return res.json({ success: true });
+	} catch (error) {
+		return res.json({
+			success: false,
+			message: error.message,
+		});
 	}
 };
