8df1e2e95ece11fded8b6f0241a78ea0ed7b3bca
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-06-14 18:22:44 +0800

    feature: add reset password functionality

--- client/src/pages/resetPassword.jsx
+++ client/src/pages/resetPassword.jsx
@@ -1,7 +1,137 @@
-import React from "react";
+import { Image, LockIcon, Mail } from "lucide-react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ResetPassword = () => {
-	return <div>ResetPassword</div>;
+	const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [isEmailSent, setIsEmailSent] = useState("");
+	const [otp, setOtp] = useState(0);
+	const [isOtpSubmitted, setIsOtpSubmitted] = useState(false);
+	const inputRefs = React.useRef([]);
+
+	const handleInput = (e, index) => {
+		if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+			inputRefs.current[index + 1].focus();
+		}
+	};
+
+	// Delete functionality
+	const handleKeyDown = (e, index) => {
+		if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+			// Move focus to prev. key
+			inputRefs.current[index - 1].focus();
+		}
+	};
+	// Handle automatic copy-paste functionality
+	const handlePaste = (e) => {
+		const paste = e.clipboardData.getData("text");
+		const pasteArray = paste.split("");
+		pasteArray.forEach((char, index) => {
+			if (inputRefs.current[index]) {
+				inputRefs.current[index].value = char;
+			}
+		});
+	};
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-linear-to-br from-blue-200 to-purple-400">
+			<Image
+				onClick={() => navigate("/")}
+				className="absolute left-5 sm:left-20 top-5 sm:w-32 cursor-pointer"
+			/>
+			{/* Enter Email ID */}
+			{/* Display this form by default */}
+
+			{!isEmailSent && (
+				<form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+					<h1 className="text-white text-2xl font-semibold text-center mb-4">
+						Reset Password
+					</h1>
+					<p className="text-center mb-6 text-indigo-300">
+						Enter Your Registered Email Address
+					</p>
+					<div className="flex items-center mb-4 w-full gap-3 px-5 py-2.5 rounded-full bg-[#333A5C]">
+						<Mail className="w-4 h-4" />
+						<input
+							className="bg-transparent outline-none text-white"
+							type="email"
+							placeholder="Enter Email"
+							value={email} // The variable email stores the current value of the input.
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+					</div>
+					<button className="w-full py-3 bg-linear-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-3 cursor-pointer">
+						Submit
+					</button>
+				</form>
+			)}
+
+			{/* OTP input form */}
+			{/* Only display if OTP is not submitted & Email is sent*/}
+			{!isOtpSubmitted && isEmailSent && (
+				<form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+					<h1 className="text-white text-2xl font-semibold text-center mb-4">
+						Reset Password OTP
+					</h1>
+					<p className="text-center mb-6 text-indigo-300">
+						Enter 6 digit codes sent to your email
+					</p>
+
+					{/* Input field */}
+
+					<div className="flex justify-between mb-8" onPaste={handlePaste}>
+						{Array(6)
+							.fill(0)
+							.map((_, index) => (
+								<input
+									type="text"
+									maxLength="1"
+									key={index}
+									required
+									className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+									ref={(e) => (inputRefs.current[index] = e)}
+									onInput={(e) => handleInput(e, index)} // Solve Manual Input
+									onKeyDown={(e) => handleKeyDown(e, index)}
+								/>
+							))}
+					</div>
+					<button className="w-full py-2.5 bg-linear-to-r from-indigo-500 to-indigo-900 text-white rounded-full">
+						Submit
+					</button>
+				</form>
+			)}
+
+			{/* Enter New Password Form */}
+			{/* display If OTP is submited & if email is sent*/}
+			{isOtpSubmitted && isEmailSent && (
+				<form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+					<h1 className="text-white text-2xl font-semibold text-center mb-4">
+						Reset Password
+					</h1>
+					<p className="text-center mb-6 text-indigo-300">
+						Enter Your Registered Email Address
+					</p>
+					<div className="flex items-center mb-4 w-full gap-3 px-5 py-2.5 rounded-full bg-[#333A5C]">
+						<LockIcon className="w-4 h-4" />
+						<input
+							className="bg-transparent outline-none text-white"
+							type="password"
+							placeholder="Enter New Password"
+							value={newPassword} // The variable email stores the current value of the input.
+							onChange={(e) => setNewPassword(e.target.value)}
+							required
+						/>
+					</div>
+
+					<button className="w-full py-3 bg-linear-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-3 cursor-pointer">
+						Submit
+					</button>
+				</form>
+			)}
+		</div>
+	);
 };
 
 export default ResetPassword;
