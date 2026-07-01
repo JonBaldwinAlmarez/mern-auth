39a690e4cf14ef49b4a4b2e1b8ebca2b1e47eaf2
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-07-01 11:57:49 +0800

    style: change icon, template and add delete account button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Hand, Image } from "lucide-react";
+import { Image, User } from "lucide-react";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
@@ -7,19 +7,15 @@ const Header = () => {
 
 	return (
 		<div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800">
-			<Image className="w-36 h-36 rounded-full mb-6" />
+			<User className="w-36 h-36 mb-6" />
 			<h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
 				Hey {userData ? userData.name : "Developer !"}{" "}
-				<Hand className="w-8 aspect-square" />
+				<Image className="w-8 aspect-square" />
 			</h1>
 			<h2 className="text-3xl sm:semibold mb-4">Welcome to my app</h2>
-			<p className="mb-8 max-w-mb">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi ante,
-				hendrerit quis mauris non, cursus convallis risus. Vestibulum at quam
-				tristique, malesuada leo at, pulvinar ligula.
-			</p>
-			<button className="border border-gray-500 rounded-full px-8 py-3 hover:bg-gray-100 transition-all cursor-pointer">
-				Click me
+			<p className="mb-8 max-w-mb">This App Sends OTP to your email</p>
+			<button className="border border-red-500 rounded-full px-8 py-3 hover:bg-red-100 transition-all cursor-pointer">
+				Delete Account
 			</button>
 		</div>
 	);
